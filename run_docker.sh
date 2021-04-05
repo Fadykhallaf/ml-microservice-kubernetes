7f9bb03aaefae775f2dc07f8c07f4c106f9908b8
fady khallaf <fady.khallaf@gmail.com>
2021-04-05 20:27:12 +0200

    update run docker script

--- run_docker.sh
+++ run_docker.sh
@@ -1,12 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-## Complete the following steps to get Docker running locally
-
-# Step 1:
 # Build image and add a descriptive tag
+docker build --tag=sklearn .
 
 # Step 2: 
-# List docker images
+docker images ls
 
 # Step 3: 
-# Run flask app
+docker run -p 8000:80 sklearn
+
